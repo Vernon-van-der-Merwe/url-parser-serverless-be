bdfb77f3fd0749c859a1b5007e36420f367a9ba2
Vernon van der Merwe <vernonv@webuycars.co.za>
2023-05-12 12:30:31 +0200

    working serverless api

---
+++ sam-app/.aws-sam/build/api/src/s.ts
@@ -0,0 +1,15 @@
+const serverless = require('serverless-http');
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+exports.handler = serverless(app);
+
---
+++ serverless-api/src/handlers/index.js
@@ -0,0 +1,13 @@
+var serverless = require('serverless-http');
+var express = require('express');
+var app = express();
+var port = 3000;
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+app.listen(port, function () {
+    console.log("Example app listening on port ".concat(port));
+});
+
+module.exports.app = serverless(app);
